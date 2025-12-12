8512cc9d38b6ba88f483cf9221c6c5b3e03b0b40
Macaron33-T <tarm3521@gmail.com>
2025-12-12 16:47:20 +0700

    UserView Addded

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./component/Navbar.jsx";
 import MainContent from "./component/MainContent.jsx";
 
 const App = () => {
+  const [view, setView] = useState("initial");
+
+  const renderView = () => {
+    switch (view) {
+      case "user":
+        return <UserView setView={setView} />;
+      case "initial":
+      default:
+        return <MainContent setView={setView} />;
+    }
+  };
+
   return (
     <div className="min-h-screen">
       <Navbar />
-      <MainContent />
+      {renderView()}
     </div>
   );
 };
-
 export default App;
