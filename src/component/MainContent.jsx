8d7141e9f22920bbf666f0456a11ebb3b01564cc
Macaron33-T <tarm3521@gmail.com>
2025-12-12 15:59:03 +0700

    Fixed Font

--- src/component/MainContent.jsx
+++ src/component/MainContent.jsx
@@ -6,11 +6,11 @@ const MainContent = () => {
         <br />
         React - Assessment
       </div>
-      <div className="flex space-x-10">
-        <button className="px-8 py-3 text-base border border-gray-300 rounded-md bg-white shadow-md cursor-pointer font-medium transition duration-150 hover:bg-gray-100">
+      <div className="flex space-x-30">
+        <button className="px-8 py-3 text-base border border-gray-300 rounded-md bg-white shadow-md cursor-pointer font-bold transition duration-150 hover:bg-gray-100">
           User Home View
         </button>
-        <button className="px-8 py-3 text-base border border-gray-300 rounded-md bg-white shadow-md cursor-pointer font-medium transition duration-150 hover:bg-gray-100">
+        <button className="px-8 py-3 text-base border border-gray-300 rounded-md bg-white shadow-md cursor-pointer font-bold transition duration-150 hover:bg-gray-100 ">
           Admin Home View
         </button>
       </div>
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,15 +1,15 @@
 const Navbar = () => {
   return (
-    <nav className="flex justify-end items-center p-4 border-b border-gray-300 bg-white">
+    <nav className="flex justify-end items-center p-4 border-b border-gray-300 bg-gray-100">
       <a
         href="#"
-        className="ml-5 text-base font-normal text-black no-underline cursor-pointer"
+        className="ml-5 text-base font-bold text-black no-underline cursor-pointer"
       >
         Owner
       </a>
       <a
         href="#"
-        className="ml-5 text-base font-normal text-black no-underline cursor-pointer"
+        className="ml-5 text-base font-bold text-black no-underline cursor-pointer"
       >
         Home
       </a>
